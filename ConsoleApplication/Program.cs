edf63d9ccdff9f0d7233a43e8262b8a540706663
marencamille19 <ste18009@byui.edu>
2020-01-12 21:10:52 -0700

    First attempt at C# application

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -17,7 +17,27 @@ namespace ConsoleApplication
 
             Console.WriteLine("My name is {0}, and I am from {1}.", name, location);
             Console.WriteLine("Date is {0}", date);
-
+            
+            //Still need to add the countdown to Christmas
+        
+        }
+        class GlazerCalc
+        {
+            static void Main()
+            {
+                double width, height, woodLength, glassArea;
+                string widthString, heightString;
+                widthString = Console.ReadLine();
+                width = double.Parse(widthString);
+                heightString = Console.ReadLine();
+                height = double.Parse(heightString);
+                woodLength = 2 * (width + height) * 3.25;
+                glassArea = 2 * (width * height);
+                Console.WriteLine("The length of the wood is " +
+                woodLength + " feet");
+                Console.WriteLine("The area of the glass is " +
+                glassArea + " square meters");
+            }
         }
     }
 }
