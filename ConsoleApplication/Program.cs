932990740d78be8b1acac2a97f5ead7b0520c320
marencamille19 <ste18009@byui.edu>
2020-01-12 21:01:40 -0700

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Maren Stewart";
+            string location = "Enumclaw, WA";
+            var dateAndTime = DateTime.Now;
+            var date = dateAndTime.Date;
+
+            Console.WriteLine("My name is {0}, and I am from {1}.", name, location);
+            Console.WriteLine("Date is {0}", date);
+
+        }
+    }
+}
