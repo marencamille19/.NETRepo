c4508d349862dd9e5b05696a1140dd2fbb0615c3
marencamille19 <ste18009@byui.edu>
2020-01-14 14:31:13 -0700

    Finished Week 01 assignment!

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -12,32 +12,36 @@ namespace ConsoleApplication
         {
             string name = "Maren Stewart";
             string location = "Enumclaw, WA";
-            var dateAndTime = DateTime.Now;
-            var date = dateAndTime.Date;
-
-            Console.WriteLine("My name is {0}, and I am from {1}.", name, location);
-            Console.WriteLine("Date is {0}", date);
             
+            var currentDate = DateTime.Now;
+
+            var christmasDate = new DateTime(2020, 12, 25);
+            var countdown = christmasDate - currentDate;
+
+            Console.WriteLine($"My name is {name}, and I am from {location}.");
+            Console.WriteLine("The current date is: " + currentDate.ToString("d"));
+            Console.WriteLine("Number of days until Christmas: " + countdown.Days);
+
+            //timespan object to convert countdown variable to number of days
             //Still need to add the countdown to Christmas
-        
+
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            Console.Write("Please enter a width: ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            Console.Write("Please enter a height: ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            Console.WriteLine("The length of the wood is " +
+            woodLength + " feet");
+            Console.WriteLine("The area of the glass is " +
+            glassArea + " square meters");
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
-        class GlazerCalc
-        {
-            static void Main()
-            {
-                double width, height, woodLength, glassArea;
-                string widthString, heightString;
-                widthString = Console.ReadLine();
-                width = double.Parse(widthString);
-                heightString = Console.ReadLine();
-                height = double.Parse(heightString);
-                woodLength = 2 * (width + height) * 3.25;
-                glassArea = 2 * (width * height);
-                Console.WriteLine("The length of the wood is " +
-                woodLength + " feet");
-                Console.WriteLine("The area of the glass is " +
-                glassArea + " square meters");
-            }
-        }
-    }
+    }    
 }
